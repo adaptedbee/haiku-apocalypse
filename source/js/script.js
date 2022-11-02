383c4c44d4d8105b582ed6a6e7e3df0b5805ff7c
Mikhail Savotikov <adaptedbee@yandex.ru>
2022-11-03 00:31:11 +0300

    Заполняем хокку из скрипта

--- source/js/script.js
+++ source/js/script.js
@@ -13,3 +13,28 @@ const initAboutPopup = () => {
 };
 
 initAboutPopup();
+
+const fillInitialText = () => {
+  const TEST_TEXT = [
+    ['На мёртвой', 'ветке'],
+    ['чернеет', 'ворон'],
+    ['осенний', 'вечер']
+  ];
+  let htmlString = '';
+  const poem = document.querySelector('.js-poem');
+
+  TEST_TEXT.forEach(line => {
+    htmlString += '<p class="poem__line">';
+    line.forEach((word, index) => {
+      if (index > 0) {
+        htmlString += ' ';
+      }
+      htmlString += `<span>${word}</span>`;
+    });
+    htmlString += '</p>';
+  });
+
+  poem.innerHTML = htmlString;
+};
+
+fillInitialText();
