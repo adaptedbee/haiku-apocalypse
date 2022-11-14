5fa3f99d28b89d075e75b1b201af4fb13318b8cb
Mikhail Savotikov <adaptedbee@yandex.ru>
2022-11-14 21:00:03 +0300

    отображаем картинку пропорционально экрану

--- source/js/data/images.js
+++ source/js/data/images.js
@@ -1,4 +1,4 @@
 export const IMAGES = [
-  'https://images.unsplash.com/photo-1666726273787-2630dc7ea8c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-  'https://images.unsplash.com/photo-1666967977372-9786f2074fef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  'https://images.unsplash.com/photo-1666726273787-2630dc7ea8c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=100',
+  'https://images.unsplash.com/photo-1666967977372-9786f2074fef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=100'
 ];
--- source/js/logic/fillInitialImage.js
+++ source/js/logic/fillInitialImage.js
@@ -7,16 +7,25 @@ const getTodayImage = () => {
 };
 
 export const fillInitialImage = () => {
-  const windowW = window.innerWidth;
-  const windowH = window.innerHeight;
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
   let img;
 
   window.setup = () => {
-    createCanvas(windowW, windowH);
+    createCanvas(windowWidth, windowHeight);
     img = loadImage(getTodayImage());
   }
 
   window.draw = () => {
-    image(img, 0, 0);
+    // отображаем картинку пропорционально экрану
+    const coefW = windowWidth / img.width;
+    const heightShift = img.height * coefW - windowHeight;
+    if (heightShift >= 0) {
+      image(img, 0, -heightShift/2, windowWidth, img.height * coefW);
+    } else {
+      const coefH = windowHeight / img.height;
+      const widthShift = img.width * coefH - windowWidth;
+      image(img, -widthShift/2, 0, img.width * coefH, windowHeight);
+    }
   }
 };
