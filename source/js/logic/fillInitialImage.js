b2f22d254afcf2c7517087b0981a97d8b9e4a765
adaptedbee <adaptedbee@yandex.ru>
2022-11-19 22:18:15 +0300

    Подключаем класс Glitch

--- source/js/logic/fillInitialImage.js
+++ source/js/logic/fillInitialImage.js
@@ -1,4 +1,5 @@
 import { IMAGES } from "../data/images.js";
+import { Glitch } from "./glitch.js";
 
 const getTodayImage = () => {
   const date = new Date();
@@ -10,22 +11,31 @@ export const fillInitialImage = () => {
   const canvasWidth = window.innerWidth < 768 ? 176 : 300;
   const canvasHeight = window.innerWidth < 768 ? 125 : 212;
   let img;
+  let glitch;
 
   window.setup = () => {
     createCanvas(canvasWidth, canvasHeight);
-    img = loadImage(getTodayImage());
+    img = loadImage(getTodayImage(), () => {
+      glitch = new Glitch(img);
+    });
   }
 
   window.draw = () => {
+    clear();
+
     // отображаем картинку пропорционально экрану
-    const coefW = canvasWidth / img.width;
-    const heightShift = img.height * coefW - canvasHeight;
-    if (heightShift >= 0) {
-      image(img, 0, -heightShift/2, canvasWidth, img.height * coefW);
-    } else {
-      const coefH = canvasHeight / img.height;
-      const widthShift = img.width * coefH - canvasWidth;
-      image(img, -widthShift/2, 0, img.width * coefH, canvasHeight);
+    // const coefW = canvasWidth / img.width;
+    // const heightShift = img.height * coefW - canvasHeight;
+    // if (heightShift >= 0) {
+    //   image(img, 0, -heightShift/2, canvasWidth, img.height * coefW);
+    // } else {
+    //   const coefH = canvasHeight / img.height;
+    //   const widthShift = img.width * coefH - canvasWidth;
+    //   image(img, -widthShift/2, 0, img.width * coefH, canvasHeight);
+    // }
+
+    if (!!glitch) {
+      glitch.show();
     }
   }
 };
