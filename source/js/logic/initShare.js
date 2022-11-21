5d0e2aa2108a9c3c8a1ea52f284aede2cce937ed
adaptedbee <adaptedbee@yandex.ru>
2022-11-21 22:13:19 +0300

    Правки - шаринг

--- source/js/logic/initShare.js
+++ source/js/logic/initShare.js
@@ -13,15 +13,17 @@ const getCurrentPoemText = () => {
 export const initShare = () => {
   const twitterButton = document.querySelector('.js-share-twitter');
   const telegramButton = document.querySelector('.js-share-telegram');
-  const URL = 'http://haiku-apocalypse.surge.sh' || window.location.href;
-  const text = getCurrentPoemText();
+  // const URL = 'http://haiku-apocalypse.surge.sh';
+  const URL = window.location.href;
 
   twitterButton.addEventListener('click', () => {
+    const text = getCurrentPoemText();
     const url = `https://twitter.com/intent/tweet?url=${URL}/&text=${text}`;
     window.open(url, '_blank').focus();
   });
 
   telegramButton.addEventListener('click', () => {
+    const text = getCurrentPoemText();
     const url = `https://telegram.me/share/url?url=${URL}&text=${text}`;
     window.open(url, '_blank').focus();
   });
