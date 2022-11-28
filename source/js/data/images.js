0fd9ba5b083d999e766e0141bd9f154873d3e848
adaptedbee <adaptedbee@yandex.ru>
2022-11-28 19:54:15 +0300

    Хокку и картинки 3-7

--- source/js/data/images.js
+++ source/js/data/images.js
@@ -1,4 +1,9 @@
 export const IMAGES = [
   '/img/poems/0102.jpg',
-  '/img/poems/0202.jpg'
+  '/img/poems/0202.jpg',
+  '/img/poems/03.jpg',
+  '/img/poems/04.jpg',
+  '/img/poems/05.jpg',
+  '/img/poems/06.jpg',
+  '/img/poems/07.jpg'
 ];
